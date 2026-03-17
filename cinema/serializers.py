48772c4121f72497e28c982eb79068e00014b4bc
Rodion <r.demianetss@gmail.com>
2026-03-17 20:49:43 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,24 +1,62 @@
 from rest_framework import serializers
+from cinema.models import Movie, Actor, Genre, CinemaHall
 
-from cinema.models import Movie
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    actors = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Actor.objects.all(),
+        required=False
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Genre.objects.all(),
+        required=False
+    )
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        actors = validated_data.pop("actors", [])
+        genres = validated_data.pop("genres", [])
+
+        movie = Movie.objects.create(**validated_data)
+
+        movie.actors.set(actors)
+        movie.genres.set(genres)
+
+        return movie
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
-        )
-        instance.duration = validated_data.get("duration", instance.duration)
+        actors = validated_data.pop("actors", None)
+        genres = validated_data.pop("genres", None)
 
-        instance.save()
+        instance = super().update(instance, validated_data)
+
+        if actors is not None:
+            instance.actors.set(actors)
+
+        if genres is not None:
+            instance.genres.set(genres)
 
         return instance
